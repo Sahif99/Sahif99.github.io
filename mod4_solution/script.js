5d6fd2bbcf6568ed4693314440222f27ab0e196e
Sahif99 <123377203+Sahif99@users.noreply.github.com>
2023-10-04 22:57:50 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,12 +1,12 @@
 var names=new Array();
-names[0]="Yaakov";
+names[0]="Sahif";
 names[1]="John";
 names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
+names[5]="rahul";
+names[6]="karry";
+names[7]="ram";
 names[8]="laura";
 names[9]="jim";
 
